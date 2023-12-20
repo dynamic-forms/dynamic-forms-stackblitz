3fb361344bef4e50ea6dec1ec4636082ace4f1e8
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2023-12-20 10:05:22 +0100

    Improvements

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { APP_INITIALIZER, enableProdMode, importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { AppService, appInitializer } from './app/app.service';
+import { IconService } from './app/services/icon.service';
+import { appRoutes } from './app/app.routes.';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, BrowserAnimationsModule, HttpClientModule),
+    provideRouter(appRoutes),
+    AppService,
+    IconService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializer,
+      deps: [AppService],
+      multi: true,
+    },
+  ],
+}).catch(err => console.error(err));
